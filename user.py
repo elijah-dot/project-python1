3e33fabbb810de8da4cffa0f032c62116539c6d9
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-04-24 09:47:33 +0300

    created class user

---
+++ user.py
@@ -0,0 +1,6 @@
+class User:
+    user_list=[]
+    def __init__(self,username,userpassword):
+        self.username = username
+        self.userpassword = userpassword
+        # self.generated_password=generated_password 
